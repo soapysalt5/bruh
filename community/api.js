d114ba12aaf22e9981734b594d53b5401d659497
Drag00nKnight999 <drag00nknight_999.universe032@slmails.com>
2026-03-14 13:49:31 -0400

    Implement API functions for fetching comments and posts

--- community/api.js
+++ community/api.js
@@ -1,1 +1,27 @@
+//API script for fetching chat messages and creating chat messages using Drag00nKnight999/Marty-Games-Chat/ as a database.
+const OWNER = "Drag00nKnight999";
+const REPO = "Marty-Games-Chat";
 
+// load comments from an issue
+
+async function getComments(issue){
+
+const res = await fetch(
+`https://api.github.com/repos/${OWNER}/${REPO}/issues/${issue}/comments`
+);
+
+return await res.json();
+
+}
+
+// load posts
+
+async function getPosts(){
+
+const res = await fetch(
+`https://api.github.com/repos/${OWNER}/${REPO}/issues`
+);
+
+return await res.json();
+
+}
